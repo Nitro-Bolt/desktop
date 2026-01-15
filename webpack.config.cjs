ceb9d44366a3023cd06482f974d4cdcec2b0415e
Cubester <cubester.spamlegends@gmail.com>
2026-01-14 21:19:33 -0500

    Revert "Fixed issues with linking scratch-gui locally"
    This reverts commit b6a916caad966b2304b9e1ffc33565c112efc971.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -94,8 +94,7 @@ module.exports = [
             alias: {
                 'scratch-gui$': path.resolve(__dirname, 'node_modules/scratch-gui/src/index.js'),
                 'scratch-render-fonts$': path.resolve(__dirname, 'node_modules/scratch-gui/src/lib/tw-scratch-render-fonts'),
-            },
-            modules: ['node_modules', 'node_modules/scratch-gui/node_modules']
+            }
         }
     },
 
@@ -115,9 +114,6 @@ module.exports = [
                     }
                 ]
             })
-        ],
-        resolve: {
-            modules: ['node_modules', 'node_modules/scratch-gui/node_modules']
-        }
+        ]
     }
 ];
